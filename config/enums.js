086e29a03a88067aab111e8e85872075c805265a
qlcid <qlcid@naver.com>
2020-08-11 06:22:37 +0900

    DOCS: books API 수정

--- config/enums.js
+++ config/enums.js
@@ -1,3 +1,22 @@
 export default {
   socialLoginType: ['kakao', 'apple'],
-}
+  colorType: [
+    'NAVY',
+    'GRAY',
+    'MINT',
+    'PINK',
+    'LEMON',
+    'BLUE',
+    'PLUM',
+    'ORANGE',
+    'BROWN',
+    'GREEN',
+    'IVORY',
+    'PURPLE',
+    'RED',
+    'PEACH',
+    'BLACK',
+  ],
+  time: ['촉촉한 새벽', '새로운 아침', '나른한 낮 시간', '빛나는 오후'],
+  tag: ['활발한', '쓸쓸한', '스릴있는'], // 태그 fix 되면 수정
+};
