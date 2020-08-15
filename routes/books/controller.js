2eb7ff23cbdbd79072b9f9eb8005cc4691690ab4
qlcid <qlcid@naver.com>
2020-08-15 21:23:38 +0900

    FEATURE: book search API 추가

--- routes/books/controller.js
+++ routes/books/controller.js
@@ -1,9 +1,36 @@
 import { validationResult } from 'express-validator';
+import xml2js from 'xml2js';
 
+import getBookInfo from '../../helpers/naver';
 import enums from '../../config/enums';
 import models from '../../models';
 import { logger } from '../../config/winston';
 import { successRes, errorRes } from '../../helpers/response';
+
+exports.searchBook = async (req, res) => {
+  try {
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //   return errorRes(req, res, '40000');
+    // }
+    const { title } = req.params;
+    const bookItems = await getBookInfo(title);
+
+    let items;
+    xml2js.parseString(bookItems, { explicitArray: false }, (err, jsonResult) => {
+      if (!err) {
+        items = jsonResult.rss.channel.item;
+      } else {
+        logger.error(err);
+      }
+    });
+
+    return successRes(req, res, items);
+  } catch (e) {
+    logger.error(e);
+    return errorRes(req, res);
+  }
+};
 
 exports.postBook = async (req, res) => {
   // const userId = req.user.id;
@@ -85,6 +112,7 @@ exports.getBook = async (req, res) => {
           as: 'books',
           where: { bookId },
           attributes: [],
+          order: [['tagId', 'DESC']],
         },
       ],
       attributes: ['name'],
--- routes/books/index.js
+++ routes/books/index.js
@@ -8,6 +8,8 @@ import bookValidator from './validator';
 
 const router = express.Router();
 
+router.get('/search/:title', wrap(bookController.searchBook));
+
 router.post('/', checkSchema(bookValidator.postValidator), wrap(bookController.postBook));
 router.get('/:id', wrap(bookController.getBook));
 router.delete('/:id', checkSchema(bookValidator.deleteValidator), wrap(bookController.deleteBook));
